39fa739a1b7b9003a95084171443566bbd0618e2
smtifahim <smtifahim@gmail.com>
2023-11-21 11:19:20 -0800

    updated query

--- sckan-explorer/js/sparql-query-texts.js
+++ sckan-explorer/js/sparql-query-texts.js
@@ -46,12 +46,12 @@ SELECT DISTINCT ?Neuron_ID ?A_IRI ?A_Label ?B_IRI ?B_Label ?C_IRI ?C_Label ?Targ
     ?Neuron_ID (ilxtr:hasAxonTerminalLocation | ilxtr:hasAxonSensoryLocation) ?B_IRI.
                 ?B_IRI (rdfs:label) ?B_Label.
 
-   # OPTIONAL{?B_IRI rdfs:subClassOf+ [rdf:type owl:Restriction ;
-   #                                 owl:onProperty partOf: ; 
-   #                                 owl:someValuesFrom ?Target_Organ_IRI].
+    OPTIONAL{?B_IRI rdfs:subClassOf+ [rdf:type owl:Restriction ;
+                                    owl:onProperty partOf: ; 
+                                    owl:someValuesFrom ?Target_Organ_IRI].
 
 
-    OPTIONAL { ?B_IRI ilxtr:isPartOf ?Target_Organ_IRI.
+   # OPTIONAL { ?B_IRI ilxtr:isPartOf ?Target_Organ_IRI.
     ?Target_Organ_IRI rdfs:label ?Target_Organ_Label
                     
     FILTER (?Target_Organ_Label in ( 'heart', 'ovary', 'brain', 'urethra', 'esophagus', 'skin of body', 'lung', 'liver', 
