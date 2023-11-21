1b898d7d8875e5384d68d3f7d339ee69bcce97b0
smtifahim <smtifahim@gmail.com>
2023-11-21 14:39:57 -0800

    updated stardog endpoint

--- sckan-explorer/js/stardog-db-query.js
+++ sckan-explorer/js/stardog-db-query.js
@@ -10,11 +10,9 @@ const conn = new stardogjs.Connection({
     username: 'SPARC',
     password: 'RCvp9tKzTdxg42py',
     //endpoint: 'https://stardog.scicrunch.io:5821',
-    endpoint: 'https://sd-5f6e22c6.stardog.cloud:5820',
-    // endpoint: 'https://sd-63f05fc2.stardog.cloud:5820',
+      endpoint: 'https://sd-c1e74c63.stardog.cloud:5820',
+    // endpoint: 'https://sd-5f6e22c6.stardog.cloud:5820',
     // endpoint: 'https://sckan-stardog.scicrunch.io',
-
-
 
   });
 
